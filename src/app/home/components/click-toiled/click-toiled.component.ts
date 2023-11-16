ef1b9a8fb71384c95798af5e1c23fc2159699ec0
valerioprds <v.paradiso89@gmail.com>
2023-11-17 00:05:06 +0100

    styling

--- src/app/home/components/click-toiled/click-toiled.component.ts
+++ src/app/home/components/click-toiled/click-toiled.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { GameData, PostApiResp } from '@app/_interfaces/interfaces';
-import { ApiServiceService } from '@app/_services/api-service.service';
-import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-click-toiled',
@@ -13,37 +10,12 @@ export class ClickToiledComponent {
   flushCount = 0;
   waterUsedPerFlush = 10; // Litros de agua por descarga
 
-  constructor(
-    public apiService: ApiServiceService,
-  ) {
-
-  }
-
   incrementCount(): void {
     this.flushCount++;
-    this.onToiletClick();
   }
 
   calculateWaterUsage(): number {
     return this.flushCount * this.waterUsedPerFlush;
-  }
-
-  onToiletClick() {
-    let waterConsumption: number = this.calculateWaterUsage();
-    let gameData: GameData = this.apiService.createApiJsonObject("bathroom", waterConsumption);
-
-    let apiResp: PostApiResp;
-    this.apiService.sendGameData(gameData)
-      .pipe(
-        finalize(() => {
-          console.log(apiResp)
-          this.apiService.getUserData();
-        }
-        )
-      )
-      .subscribe((resp) => apiResp = resp);
-
-      this.apiService.getUser().subscribe((resp) => console.log('get test', resp))
-  }
+  }
 
 }
