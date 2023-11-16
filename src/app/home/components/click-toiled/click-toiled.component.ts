2b70c6da5f86e8b4ff0a1852b9509dd4d7ad5acb
laradelrio <lara.delrio333@gmail.com>
2023-11-16 22:37:58 +0100

    component logic with back-part 1

--- src/app/home/components/click-toiled/click-toiled.component.ts
+++ src/app/home/components/click-toiled/click-toiled.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { GameData, PostApiResp } from '@app/_interfaces/interfaces';
+import { ApiServiceService } from '@app/_services/api-service.service';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-click-toiled',
@@ -10,12 +13,37 @@ export class ClickToiledComponent {
   flushCount = 0;
   waterUsedPerFlush = 10; // Litros de agua por descarga
 
+  constructor(
+    public apiService: ApiServiceService,
+  ) {
+
+  }
+
   incrementCount(): void {
+    console.log('hola')
     this.flushCount++;
+   
+    this.onToiletClick();
   }
 
   calculateWaterUsage(): number {
     return this.flushCount * this.waterUsedPerFlush;
+  }
+
+  onToiletClick() {
+    let waterConsumption: number = this.calculateWaterUsage();
+    let gameData: GameData = this.apiService.createApiJsonObject("bathroom", waterConsumption);
+
+    let apiResp: PostApiResp;
+    this.apiService.sendGameData(gameData)
+      .pipe(
+        finalize(() => {
+          console.log(apiResp)
+          this.apiService.getUserData()
+        }
+        )
+      )
+      .subscribe((resp) => apiResp = resp);
   }
 
 }
--- src/app/home/components/status-progress-bar/status-progress-bar.component.ts
+++ src/app/home/components/status-progress-bar/status-progress-bar.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '@app/_services/api-service.service';
 
 @Component({
   selector: 'app-status-progress-bar',
   templateUrl: './status-progress-bar.component.html',
   styleUrls: ['./status-progress-bar.component.css']
 })
-export class StatusProgressBarComponent {
+export class StatusProgressBarComponent implements OnInit {
   maxValue = 10;  // Valor máximo ahora es 10
   statusBar = 5;  // Nuevo valor que reemplaza a currentValue
   statusBarString = '85%'
 
+  constructor(
+    private apiService: ApiServiceService,
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.setBarValues()
+  }
+
   get progressWidth() {
     return (this.statusBar / this.maxValue) * 100 + '%';
+  }
+
+  setBarValues() {
+    this.apiService.userTotalWaterUsage
+    .subscribe( (resp) => {
+      this.statusBar = resp
+      this.statusBarString = `${this.statusBar}%` })
   }
 }
