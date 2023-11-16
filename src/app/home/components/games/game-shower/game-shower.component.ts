1d4a4268f31354583011194b6c54907bf5877e66
valerioprds <v.paradiso89@gmail.com>
2023-11-16 17:45:08 +0100

    timer works

--- src/app/home/components/games/game-shower/game-shower.component.ts
+++ src/app/home/components/games/game-shower/game-shower.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game-shower',
   templateUrl: './game-shower.component.html',
   styleUrls: ['./game-shower.component.css']
 })
-export class GameShowerComponent {
+export class GameShowerComponent implements OnDestroy {
+  private timeInSeconds: number = 0;
+  isRunning: boolean = false;
+  private timerSubscription?: Subscription;
 
+  get time(): string {
+    const minutes = Math.floor(this.timeInSeconds / 60);
+    const seconds = this.timeInSeconds % 60;
+    return `${minutes}m : ${seconds.toString().padStart(2, '0')}s`;
+  }
+
+  startTimer() {
+    if (!this.isRunning) {
+      this.isRunning = true;
+      const timer = interval(1000);
+      this.timerSubscription = timer.subscribe(() => {
+        this.timeInSeconds++;
+      });
+    }
+  }
+
+  stopTimer() {
+    this.isRunning = false;
+    this.timerSubscription?.unsubscribe();
+    this.timerSubscription = undefined;
+    this.timeInSeconds = 0;
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  get timerColor(): string {
+    return this.timeInSeconds >= 300 ? 'red' : 'black'; // Cambia a rojo después de 5 minutos
+  }
 }
